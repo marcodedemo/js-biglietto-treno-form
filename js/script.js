b352664eb20c1bc39021637ee3d1efbca2e39984
marcodedemo <marco.dedemo@gmail.com>
2023-02-23 16:01:23 +0100

    feat: milestone 2

--- js/script.js
+++ js/script.js
@@ -28,10 +28,18 @@ let submitButton = document.getElementById("submit-button");
 submitButton.addEventListener("click", function(){
 
 
-    // memorizzo numero di km da percorre e età
+    // html display info 
 
+    document.getElementById("incomplete-inputs").style.display = "none";
+    document.getElementById("hidden-info").style.display = "block";
+
+
+    // memorizzo numero di km da percorre, l'età e il nome del passeggero
+
+    let userName = document.getElementById("input-name").value;
     let kilometers = document.getElementById("input-km").value;
     let userAge = document.getElementById("input-age").value;
+    
 
 
     // calcolo il prezzo base e dichiaro la variabile prezzo scontato
@@ -43,21 +51,44 @@ submitButton.addEventListener("click", function(){
     // se l'età è minore di 18 anni applico uno sconto del 20%
 
     if(userAge < 18){
+
         discountedPrice = (fullPrice - fullPrice / 100 * 20);
         console.log("hai ottenuto uno sconto del 20% in quanto minorenne, il prezzo totale del tuo biglietto è di " + discountedPrice.toFixed(2) + "€");
+
+
+        document.getElementById("printed-name").innerHTML = userName;
+        document.getElementById("printed-offer").innerHTML = "20%";
+        document.getElementById("printed-carriage").innerHTML = Math.floor(Math.random() * 10 +1);
+        document.getElementById("printed-code").innerHTML = Math.floor(Math.random() * 10000 +1);
+        document.getElementById("printed-price").innerHTML = discountedPrice.toFixed(2) + " €";
+
     
 
     // se l'età è maggiore o uguale a 65 anni applico uno sconto del 40%
 
     } else if (userAge >= 65){
+
         discountedPrice = (fullPrice - fullPrice / 100 * 40);
         console.log("hai ottenuto uno sconto del 40% in quanto over 65, il prezzo totale del tuo biglietto è di " + discountedPrice.toFixed(2) + "€");
+
+
+        document.getElementById("printed-name").innerHTML = userName;
+        document.getElementById("printed-offer").innerHTML = "40%";
+        document.getElementById("printed-carriage").innerHTML = Math.floor(Math.random() * 10 +1);
+        document.getElementById("printed-code").innerHTML = Math.floor(Math.random() * 10000 +1);
+        document.getElementById("printed-price").innerHTML = discountedPrice.toFixed(2) + " €";
 
 
     // altrimenti il bigliettò sarà venduto a prezzo pieno 
 
     } else {
         console.log("il prezzo totale del tuo biglietto è di " + fullPrice.toFixed(2) + "€");
+
+        document.getElementById("printed-name").innerHTML = userName;
+        document.getElementById("printed-offer").innerHTML = "/";
+        document.getElementById("printed-carriage").innerHTML = Math.floor(Math.random() * 10 +1);
+        document.getElementById("printed-code").innerHTML = Math.floor(Math.random() * 10000 +1);
+        document.getElementById("printed-price").innerHTML = fullPrice.toFixed(2) + " €";
     }
 })
 
